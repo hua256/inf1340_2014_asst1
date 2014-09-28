1a6e4c8ebe11ba3212733dab3937d0ec59f9157c
Christopher <chris.piche@gmail.com>
2014-09-28 15:06:25 -0400

    First draft for Exercise 1 at meeting together

--- exercise1.py
+++ exercise1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-""" Assignment 1, Exercise 1, INF1340, Fall, 2014. Grade to gpa conversion
+""" Assignment 1, Exercise 1, INF1340, Fall, 2014. Grade-to-GPA conversion.
 
 This module contains one function grade_to_gpa. It can be passed a parameter
 that is an integer (0-100) or a letter grade (A+, A, A-, B+, B, B-, or FZ). All
@@ -11,25 +11,23 @@ Example:
 
 """
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
+__author__ = "Joshua Chisholm and Christopher Piche"
+__email__ = "josh.chisholm@mail.utoronto.ca and christopher.piche@mail.utoronto.ca"
 
-__copyright__ = "2014 Susan Sim"
+__copyright__ = "2014 Joshua Chisholm an Christopher Piche"
 __license__ = "MIT License"
 
 __status__ = "Prototype"
 
 # imports one per line
 
-print ('test')
-
 def grade_to_gpa(grade):
     """
-    Returns the UofT Graduate GPA for a given grade.
+    Returns the UofT Graduate GPA for a given grade
 
     :param:
         grade (integer or string): Grade to be converted
-            If integer, accepted values are 0-100.
+            If integer, accepted values are 0-100
             If string, accepted values are A+, A, A-, B+, B, B-, FZ
 
     :return:
@@ -41,28 +39,42 @@ def grade_to_gpa(grade):
         ValueError if parameter is out of range
     """
 
-print ('test21')
-    letter_grade = ""
-    gpa = 0.0
+    letter_grade = {"A+": 4.0, "A": 4.0, "A-": 3.7, "B+": 3.3, "B": 3.0, "B-": 2.7, "FZ": 0.0}
 
     if type(grade) is str:
-        print ("letter") # remove this line once the code is implemented
         # check that the grade is one of the accepted values
         # assign grade to letter_grade
+
+        gpa = letter_grade[grade]
+
     elif type(grade) is int:
-        print("mark") # remove this line once the code is implemented
         # check that grade is in the accepted range
         # convert the numeric grade to a letter grade
         # assign the value to letter_grade
-        # hint: letter_grade = mark_to_letter(grade)
+
+        if grade < 0:
+            # raise a ValueError exception
+            raise ValueError("Entered grade can not be less than 0")
+        elif grade <= 69:
+            gpa = 0.0
+        elif grade <= 72:
+            gpa = 2.7
+        elif grade <= 76:
+            gpa = 3.0
+        elif grade <= 79:
+            gpa = 3.3
+        elif grade <= 84:
+            gpa = 3.7
+        elif grade <= 100:
+            gpa = 4.0
+        else:
+            # raise a ValueError
+            raise ValueError("Entered grade can not be greater than 100")
+
+
     else:
         # raise a TypeError exception
         raise TypeError("Invalid type passed as parameter")
-
-    # write a long if-statement to convert letter_grade
-    # assign the value to gpa
-    if letter_grade == "A":
-        gpa = 4.0
 
     return gpa
 
